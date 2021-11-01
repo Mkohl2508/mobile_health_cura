e22ac1d3021be24fdcaef2efca0d2fd205a162a9
Marlon Kohlberger <marlonkohlberger2508@gmail.com>
2021-11-01 17:28:35 +0100

    first steps for the login screen

--- Implementation/App/lib/screens/login_screen.dart
+++ Implementation/App/lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cura/shared/text_input_login_wdiget.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -10,26 +11,76 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Welcome to Cura", style: TextStyle(fontSize: 40)),
-            Column(
-              children: [
-                Text("Progress is loading..."),
-                SizedBox(
-                  height: 15,
-                ),
-                CircularProgressIndicator(),
-              ],
-            )
-          ],
-        ),
-      ),
-    ));
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          Color(0xff37ffa5),
+          Color(0xff3798ff),
+        ],
+        begin: Alignment(0, 1),
+        end: Alignment(-1.0, -1),
+      )),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text("Welcome to Cura",
+                      style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          shadows: [
+                            Shadow(
+                              offset: Offset(0, 4.0),
+                              blurRadius: 4.0,
+                              color: Color.fromARGB(64, 0, 0, 0),
+                            )
+                          ])),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Icon(
+                    Icons.account_circle,
+                    size: 150,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  TextInputLogin(
+                    icon: Icon(
+                      Icons.person,
+                      color: Color(0x7001A1DB),
+                      size: 25,
+                    ),
+                    color: Color(0x7001A1DB),
+                    hint: "Username",
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextInputLogin(
+                    isPassword: true,
+                    icon: Icon(
+                      Icons.lock,
+                      color: Color(0x7001A1DB),
+                      size: 25,
+                    ),
+                    color: Color(0x7001A1DB),
+                    hint: "Password",
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
   }
 }
---
+++ Implementation/App/lib/shared/text_input_login_wdiget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class TextInputLogin extends StatefulWidget {
+  final Icon? startIcon;
+  final Color? verticalDevColor;
+  final String? textHint;
+  final bool isPassword;
+  const TextInputLogin(
+      {Key? key,
+      Icon? icon,
+      Color? color,
+      String? hint,
+      bool isPassword = false})
+      : startIcon = icon,
+        textHint = hint,
+        verticalDevColor = color,
+        // ignore: prefer_initializing_formals
+        isPassword = isPassword,
+        super(key: key);
+
+  @override
+  _TextInputLoginState createState() =>
+      _TextInputLoginState(startIcon, verticalDevColor, textHint, isPassword);
+}
+
+class _TextInputLoginState extends State<TextInputLogin> {
+  final Icon? startIcon;
+  final Color? verticalDevColor;
+  final String? textHint;
+  final bool isPassword;
+
+  _TextInputLoginState(
+      this.startIcon, this.verticalDevColor, this.textHint, this.isPassword);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: [
+          Padding(padding: const EdgeInsets.only(left: 15), child: startIcon),
+          VerticalDivider(
+            color: verticalDevColor,
+            thickness: 3,
+            indent: 13,
+            endIndent: 13,
+            width: 20,
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: TextField(
+              obscureText: isPassword,
+              enableSuggestions: !isPassword,
+              autocorrect: !isPassword,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: textHint,
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
