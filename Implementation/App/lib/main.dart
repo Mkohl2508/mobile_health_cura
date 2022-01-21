b0f43b7da789123b83f2e5c08c49f09a841bae1a
Marlon Kohlberger <marlonkohlberger2508@gmail.com>
2022-01-21 11:17:54 +0100

    adding firebase functions and pushnotifications

--- Implementation/App/lib/main.dart
+++ Implementation/App/lib/main.dart
@@ -1,13 +1,42 @@
-import 'package:cura/model/residence/residence.dart';
+import 'dart:async';
+import 'dart:io';
+
 import 'package:cura/screens/login_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'globals.dart' as globals;
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'cura_notification_channel_id', // id
+    'Cura wound notifications', // title
+    importance: Importance.high,
+    playSound: true);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('A bg message just showed up :  ${message.messageId}');
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
   runApp(const CuraApp());
 }
 
@@ -18,7 +47,7 @@ class CuraApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Cura',
+        title: 'CURA',
         theme: ThemeData(
           // This is the theme of your application.
           //
